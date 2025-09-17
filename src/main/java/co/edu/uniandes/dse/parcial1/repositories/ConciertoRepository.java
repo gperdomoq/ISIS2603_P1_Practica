ab10be70782dac46c5dc695efa5542423585dbe0
Gabriela <g.perdomoq@uniandes.edu.co>
2025-09-17 13:36:28 -0500

    services

--- src/main/java/co/edu/uniandes/dse/parcial1/repositories/ConciertoRepository.java
+++ src/main/java/co/edu/uniandes/dse/parcial1/repositories/ConciertoRepository.java
@@ -1,5 +1,7 @@
 package co.edu.uniandes.dse.parcial1.repositories;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import co.edu.uniandes.dse.parcial1.entities.ConciertoEntity;
 
 @Repository
 public interface ConciertoRepository extends JpaRepository<ConciertoEntity, Long> {
+
+    Object findByNombre(String nombre);
+
+    Object findByFecha(LocalDateTime fecha);
 
 }
--- src/main/java/co/edu/uniandes/dse/parcial1/services/ConciertoEstadioService.java
+++ src/main/java/co/edu/uniandes/dse/parcial1/services/ConciertoEstadioService.java
@@ -1,13 +1,46 @@
 package co.edu.uniandes.dse.parcial1.services;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.edu.uniandes.dse.parcial1.entities.ConciertoEntity;
+import co.edu.uniandes.dse.parcial1.entities.EstadioEntity;
+import co.edu.uniandes.dse.parcial1.repositories.ConciertoRepository;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 public class ConciertoEstadioService {
 
-    // Complete
+    @Autowired
+    private ConciertoRepository conciertoRepository;
+
+
+    @Transactional
+    public ConciertoEntity createConcierto(ConciertoEntity concierto) throws IllegalAccessException {
+        log.info("Inicia proceso de creación del concierto");
+
+        if (conciertoRepository.findById(concierto.getId()) != null)
+            throw new IllegalAccessException("Ya existe un concierto con el mismo Id \"" + concierto.getId() + "\"");
+
+        if (conciertoRepository.findByFecha(concierto.getFecha()) == LocalDateTime.now() && conciertoRepository.findByFecha(concierto.getFecha()) 
+            throw new IllegalAccessException("Ya existe un concierto con el mismo nombre \"" + concierto.getNombre() + "\"");
+        return conciertoRepository.save(concierto);
+    }
+
+
+    public void asignarEstadioAConcierto(ConciertoEntity concierto, EstadioEntity estadio) {
+        concierto.getEstadios().add(estadio);
+        estadio.setConcierto(concierto);
+    }
+
+    public void desasignarEstadioDeConcierto(ConciertoEntity concierto, EstadioEntity estadio) {
+        concierto.getEstadios().remove(estadio);
+        estadio.setConcierto(null);
+    }
+
+
 
 }
