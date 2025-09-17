41e33c74007bc3ed281ce1dc1b1856f8e2194615
Gabriela <g.perdomoq@uniandes.edu.co>
2025-09-17 13:19:58 -0500

    1

--- src/main/java/co/edu/uniandes/dse/parcial1/entities/ConciertoEntity.java
+++ src/main/java/co/edu/uniandes/dse/parcial1/entities/ConciertoEntity.java
@@ -1,12 +1,26 @@
 package co.edu.uniandes.dse.parcial1.entities;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
 public class ConciertoEntity extends BaseEntity {
 
     private String nombre;
+    private LocalDateTime fecha;
+    private String nombreArtista;
     private Long presupuesto;
+    private int aforo;
+
+    @PodamExclude
+    @OneToMany(mappedBy = "concierto", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<EstadioEntity> estadios = new ArrayList<>();
 }
--- src/main/java/co/edu/uniandes/dse/parcial1/entities/EstadioEntity.java
+++ src/main/java/co/edu/uniandes/dse/parcial1/entities/EstadioEntity.java
@@ -1,7 +1,10 @@
 package co.edu.uniandes.dse.parcial1.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
@@ -9,4 +12,11 @@ public class EstadioEntity extends BaseEntity {
 
     private String nombre;
     private Long precioAlquiler;
+    private int capacidad;
+    private String nombreCiudad;
+
+
+    @PodamExclude
+    @ManyToOne @JoinColumn(name = "concierto")
+    private ConciertoEntity concierto;
 }
